a75135ca7f0a11897fc45ee3b5569651819c2ea8
revened <pvarevened@gmail.com>
2025-08-13 14:17:49 +0300

    aboba

---
+++ src/main/java/com/letitbackupcreator/LetitBackUpCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.letitbackupcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetitBackUpCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetitBackUpCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letitbackupcreator/LetitBackUpCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letitbackupcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetitBackUpCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
